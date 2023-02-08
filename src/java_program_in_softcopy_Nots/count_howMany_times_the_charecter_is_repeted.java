232f4548d9688680475c968a4261d2e5fc5b9da2
Ravikiran lv <ravikiranlv16@gmail.com>
2023-02-08 15:49:03 +0530

    added

--- src/java_program_in_softcopy_Nots/count_howMany_times_the_charecter_is_repeted.java
+++ src/java_program_in_softcopy_Nots/count_howMany_times_the_charecter_is_repeted.java
@@ -22,12 +22,13 @@ import java.util.Scanner;
 public class count_howMany_times_the_charecter_is_repeted {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enetr the String ; ");
+		System.out.println("Enetr the String : ");
 		String a = sc.nextLine();
 		int count = 0;
 		char arr[] = a.toCharArray();
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] == 'a') {
+				
 				count++;
 			}
 			System.out.println(count);
